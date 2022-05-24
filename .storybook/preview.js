b9b611aea9d95a91bb16a1a11e3676bb8f51bc1e
gzamaury <gzamaury@hotmail.com>
2022-05-24 15:46:58 -0500

    - Mocking API Services

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import "../src/index.css";
 
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
+
 //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 // parameters are typically used to control the behavior of Storybook's features and addons.
 // In our case, we're going to use them to configure how the actions (mocked callbacks) are
